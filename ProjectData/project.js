aacec7af8767fd02728c8dfd5711db1359961c39
Sahil Hussain <sahilwethehelpers@gmail.com>
2023-09-17 17:15:04 +0530

    Project data JSON included

---
+++ ProjectData/project.js
@@ -0,0 +1,27 @@
+export const project_1 = {
+    "project_name": "project 1",
+    "project_description": "Project1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, incidunt! Laboriosam debitis, illo officiis cupiditate, ab ratione veniam id obcaecati repudiandae, accusamus in distinctio accusantium? Accusamus esse repellat obcaecati voluptates provident at perferendis vel exercitationem molestiae nesciunt ad assumenda reprehenderit, iste laborum officia, quam consequuntur nisi incidunt quaerat veritatis quas?",
+    "project_images": [{ "image_link": "https://images.unsplash.com/photo-1576179635662-9d1983e97e1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80" },
+    { "image_link": "https://images.unsplash.com/photo-1576179635662-9d1983e97e1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80" }],
+    "reviewer_name": "Mr Raja",
+    "review": "Project 1 Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "review_contact": "9991123459"
+}
+export const project_2 = {
+    "project_name": "project 2",
+    "project_description": "Project2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, incidunt! Laboriosam debitis, illo officiis cupiditate, ab ratione veniam id obcaecati repudiandae, accusamus in distinctio accusantium? Accusamus esse repellat obcaecati voluptates provident at perferendis vel exercitationem molestiae nesciunt ad assumenda reprehenderit, iste laborum officia, quam consequuntur nisi incidunt quaerat veritatis quas?",
+    "project_images": [{ "image_link": "https://images.unsplash.com/photo-1576179635662-9d1983e97e1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80" },
+    { "image_link": "https://images.unsplash.com/photo-1576179635662-9d1983e97e1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80" }],
+    "reviewer_name": "Mr Sajid Raja",
+    "review": "Project 2 Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "review_contact": "29991123459"
+}
+export const project_3 = {
+    "project_name": "project 3",
+    "project_description": "Project3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, incidunt! Laboriosam debitis, illo officiis cupiditate, ab ratione veniam id obcaecati repudiandae, accusamus in distinctio accusantium? Accusamus esse repellat obcaecati voluptates provident at perferendis vel exercitationem molestiae nesciunt ad assumenda reprehenderit, iste laborum officia, quam consequuntur nisi incidunt quaerat veritatis quas?",
+    "project_images": [{ "image_link": "https://images.unsplash.com/photo-1576179635662-9d1983e97e1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80" },
+    { "image_link": "https://images.unsplash.com/photo-1576179635662-9d1983e97e1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80" }],
+    "reviewer_name": "Mr Wajid Raja",
+    "review": "Project 3 Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "review_contact": "39991123459"
+}
